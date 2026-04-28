f8fd10b4b28ebae719d46ad71925a6d028454383
0x61 <63924005+KenanY1@users.noreply.github.com>
2026-04-28 18:14:52 +0200

    Implement asynchronous result handling with callback
    Added asynchronous callback handling for result processing.

--- lab2/rpc/runcl.py
+++ lab2/rpc/runcl.py
@@ -1,16 +1,28 @@
 import rpc
 import logging
+import time
 
 from context import lab_logging
 
 lab_logging.setup(stream_level=logging.INFO)
 
+def handle_result(result):
+    print(f"[Callback] Ergebnis erhalten: {result.value}")
+
+
+
 cl = rpc.Client()
 cl.run()
 
 base_list = rpc.DBList({'foo'})
-result_list = cl.append('bar', base_list)
 
-print("Result: {}".format(result_list.value))
+#Asynchronous call with callback
+cl.append('bar', base_list, callback=handle_result)
+
+
+for i in range(4):
+    print(f"[Client] macht Task{i}")
+    time.sleep(1)
 
 cl.stop()
+
