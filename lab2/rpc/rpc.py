beeda3af7de9923715c81eb3610400c6a8d72789
0x61 <63924005+KenanY1@users.noreply.github.com>
2026-04-28 18:14:15 +0200

    Enhance append method to support callbacks
    Added asynchronous handling for append method with callback support.

--- lab2/rpc/rpc.py
+++ lab2/rpc/rpc.py
@@ -1,4 +1,6 @@
 import constRPC
+import threading
+import time
 
 from context import lab_channel
 
@@ -17,20 +19,38 @@ class Client:
         self.chan = lab_channel.Channel()
         self.client = self.chan.join('client')
         self.server = None
+        self._result_threads = []
 
     def run(self):
         self.chan.bind(self.client)
         self.server = self.chan.subgroup('server')
 
     def stop(self):
+        for thread in self._result_threads:
+            thread.join()
         self.chan.leave('client')
 
-    def append(self, data, db_list):
+    def append(self, data, db_list, callback=None):
         assert isinstance(db_list, DBList)
         msglst = (constRPC.APPEND, data, db_list)  # message payload
         self.chan.send_to(self.server, msglst)  # send msg to server
-        msgrcv = self.chan.receive_from(self.server)  # wait for response
-        return msgrcv[1]  # pass it to caller
+        
+        ack_msg = self.chan.receive_from(self.server)  # wait for response
+        print("[Client] hat ACK erhalten - mache nun andere sachen weiter...")
+
+        if callback:
+            result_thread = threading.Thread(target=self.wait_for_result, args=(callback,))
+            result_thread.daemon = True
+            result_thread.start()
+            self._result_threads.append(result_thread)
+
+    def wait_for_result(self, callback):
+        print("[Client] warte nebenbei auf Ergebnis...")
+        result = self.chan.receive_from(self.server)  # wait for response
+        sender, result_data = result
+        print(f"[Client] Ergebnis erhalten: {result_data.value}")
+
+        callback(result_data)
 
 
 class Server:
@@ -51,8 +71,21 @@ class Server:
             if msgreq is not None:
                 client = msgreq[0]  # see who is the caller
                 msgrpc = msgreq[1]  # fetch call & parameters
+                
                 if constRPC.APPEND == msgrpc[0]:  # check what is being requested
-                    result = self.append(msgrpc[1], msgrpc[2])  # do local call
-                    self.chan.send_to({client}, result)  # return response
+                    data = msgrpc[1]
+                    db_list = msgrpc[2]
+
+                    print("[Server] sende ACK zurück an Client")
+                    self.chan.send_to({client}, constRPC.ACK)  # send ACK back to client
+
+                    print("[Server] bearbeite Anfrage...")
+                    time.sleep(10)
+
+                    result = self.append(data, db_list)
+                    print("[Server] sende Ergebnis zurück an Client")
+                    self.chan.send_to({client}, result)  # send result back to client
+
+
                 else:
                     pass  # unsupported request, simply ignore
